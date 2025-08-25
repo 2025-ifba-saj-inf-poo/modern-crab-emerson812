b165551c15eb09733f5d0a0999e8d387e41ebbe3
emerson812 <emersonneiva07@gmail.com>
2025-08-25 12:18:30 -0300

    Fim da tarefa

---
+++ Lobster.java
@@ -0,0 +1,7 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+ public class Lobster extends Decapoda
+{
+    public Lobster(){        
+        super("q","w",550,550);        
+    }    
+}
